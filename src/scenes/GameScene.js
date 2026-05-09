bd021cf8669510e996551369078447ec527ccdca
Erik Andersson <erikandersson@MBPsomtlhorErik.lan>
2026-05-09 21:12:07 +0200

    task 5: ObstacleSpawner-integration + sten-spawn

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { ObstacleSpawner } from '../systems/ObstacleSpawner.js';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -55,6 +56,27 @@ export default class GameScene extends Phaser.Scene {
 
     this.scrollSpeed = 200;
 
+    // Hindergenerator
+    this.obstacleSpawner = new ObstacleSpawner({
+      obstacleTypes: ['rock'],
+      minIntervalMs: 1500,
+      spawnWindowMs: 1500,
+    });
+    this.obstacles = this.physics.add.group({ allowGravity: false, immovable: true });
+
+    // Procedural sten-textur
+    const rockKey = 'obstacle-rock';
+    if (!this.textures.exists(rockKey)) {
+      const g = this.add.graphics();
+      g.fillStyle(0x808080, 1);
+      g.fillCircle(20, 20, 18);
+      g.fillStyle(0x606060, 1);
+      g.fillCircle(15, 18, 5);
+      g.fillCircle(25, 22, 4);
+      g.generateTexture(rockKey, 40, 40);
+      g.destroy();
+    }
+
     // Tap → hopp om vi står på marken.
     this.input.on('pointerdown', () => this.tryJump());
   }
@@ -65,8 +87,27 @@ export default class GameScene extends Phaser.Scene {
     }
   }
 
+  spawnObstacle(type) {
+    const w = this.scale.width;
+    const groundY = this.scale.height - 80;
+    const key = type === 'rock' ? 'obstacle-rock' : 'obstacle-rock';
+    const obstacle = this.obstacles.create(w + 50, groundY - 30, key);
+    obstacle.setVelocityX(-this.scrollSpeed);
+    obstacle.body.setSize(30, 30);
+    obstacle.setData('type', type);
+  }
+
   update(time, delta) {
-    // delta är i ms. Skrolla mark-texturen åt vänster för att simulera rörelse åt höger.
     this.ground.tilePositionX += (this.scrollSpeed * delta) / 1000;
+
+    const airborne = !(this.bike.body.blocked.down || this.bike.body.touching.down);
+    const event = this.obstacleSpawner.tick(delta, { bikeAirborne: airborne });
+    if (event) this.spawnObstacle(event.type);
+
+    // Städa hinder som åkt ut till vänster.
+    this.obstacles.children.each((obstacle) => {
+      if (obstacle && obstacle.x < -100) obstacle.destroy();
+      return true;
+    });
   }
 }
